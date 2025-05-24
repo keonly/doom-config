7a1121798e5d70af986316fae48155dc1b1c7354
keonly <70371393+keonly@users.noreply.github.com>
2025-05-24 19:15:03 +0900

    feat: set doom-symbol-font

--- config.el
+++ config.el
@@ -22,7 +22,8 @@
 ;; accept. For example:
 ;;
 (setq doom-font (font-spec :family "Iosevka Nerd Font Mono" :size 16 :weight 'regular)
-     doom-variable-pitch-font (font-spec :family "Noto Sans" :size 14))
+     doom-variable-pitch-font (font-spec :family "Noto Sans" :size 14)
+     doom-symbol-font (font-spec :family "Iosevka Nerd Font Mono" :size 16 :weight 'regular))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
