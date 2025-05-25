c1f741b99489a59aa947e94e495ed26127872999
keonly <keon.lee@proton.me>
2025-05-25 17:13:28 +0900

    feat: disable emoji in doom-modeline-modal

--- config.el
+++ config.el
@@ -25,6 +25,7 @@
      doom-variable-pitch-font (font-spec :family "Noto Sans" :size 14)
      doom-symbol-font (font-spec :family "Iosevka Nerd Font Mono" :size 16 :weight 'regular))
 (set-fontset-font t 'hangul (font-spec :family "Noto Sans CJK KR") nil 'prepend)
+
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -91,3 +92,13 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+(after! doom-modeline
+  (setq doom-modeline-modal-icon nil
+        doom-modeline-modal-modern-icon nil))
+(setq evil-normal-state-tag   " NORMAL "
+      evil-insert-state-tag   " INSERT "
+      evil-visual-state-tag   " VISUAL "
+      evil-replace-state-tag  " REPLACE "
+      evil-motion-state-tag   " MOTION "
+      evil-operator-state-tag " OPERATOR ")
